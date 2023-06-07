2bac09270aba826abd77c35272a5eda16099b28f
jubei <jubei@deemos.club>
2023-06-07 09:55:51 +0300

    partially working version

--- demagogue.py
+++ demagogue.py
@@ -4,20 +4,28 @@ from pymongo import MongoClient
 from discord.ext import commands
 from dotenv import load_dotenv
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 from datetime import datetime, timedelta
 
 load_dotenv()
+
 TOKEN = os.getenv('YOUR_BOT_TOKEN') # Your Discord bot token
 MONGO_CONNECTION_STRING = os.getenv('MONGO_CONNECTION_STRING') # Your MongoDB connection string
+ROLE_ID = 1115617370745077800  # The ID of the role to be added.
 
 mongo_client = MongoClient(MONGO_CONNECTION_STRING)
 db = mongo_client['deemos'] # Your MongoDB database
 
 intents = discord.Intents.all()  # This line enables all intents.
 bot = commands.Bot(command_prefix='!', intents=intents)
-scheduler = AsyncIOScheduler()
+jobstores = {'default': SQLAlchemyJobStore(url='sqlite:///jobs.sqlite')}
+scheduler = AsyncIOScheduler(jobstores=jobstores)
 
-async def list_members(guild, channel):
+@bot.command()
+async def list_now(guild_id, channel_id):
+    guild = bot.get_guild(guild_id)
+    channel = bot.get_channel(channel_id)
+
     voice_channels = guild.voice_channels
     voice_members = []
     
@@ -28,6 +36,35 @@ async def list_members(guild, channel):
     voice_members = '\n'.join(voice_members)
     await channel.send(f'Currently in Voice Channels: \n{voice_members}')
 
+
+@bot.event
+async def on_scheduled_event_user_add(event, user):
+    # Split the event name string into parts
+    event_parts = event.name.split(" vs ")
+
+    # Check if the string has been correctly split into three parts
+    if len(event_parts) == 3:
+        opponent_name = event_parts[2].lower()
+
+        guild = bot.get_guild(event.guild_id)
+
+        # Find the role that contains the opponent name as a substring
+        role = discord.utils.find(lambda r: opponent_name in r.name.lower(), guild.roles)
+
+        if role is not None:
+            member = guild.get_member(user.id)
+
+            if role not in member.roles:
+                await member.add_roles(role)
+
+@bot.event
+async def on_scheduled_event_user_remove(event, user):
+    guild = bot.get_guild(event.guild_id)
+    role = discord.utils.get(guild.roles, id=ROLE_ID)
+
+    member = guild.get_member(user.id)
+    if role in member.roles:
+        await member.remove_roles(role)
 
 @bot.event
 async def on_ready():
@@ -50,10 +87,21 @@ async def list(ctx, time: str):
 
         run_date = datetime.now().replace(hour=time_obj.hour, minute=time_obj.minute, second=time_obj.second)
 
-        scheduler.add_job(list_members, 'date', run_date=run_date, args=[ctx.guild, ctx.channel])
+        scheduler.add_job(list_now, 'date', run_date=run_date, args=[ctx.guild.id, ctx.channel.id])
         await ctx.send(f'Listing of members in voice channels scheduled at {time_obj.strftime(time_format)}.')
 
     except ValueError:
         await ctx.send(f"Invalid time format. Please use 24-hour format: HH:MM.")
+
+@bot.command()
+async def list_jobs(ctx):
+    jobs = scheduler.get_jobs()
+    response = ''
+    for i, job in enumerate(jobs, start=1):
+        response += f"{i}. {job.name} scheduled at {job.next_run_time} \n"
+    if response:
+        await ctx.send(response)
+    else:
+        await ctx.send("No jobs scheduled.")
 
 bot.run(TOKEN)
